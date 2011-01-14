76cb34706b90885bfc5b42e1a49bbefcc39faf0d
Ryan Leslie <rleslie@partnerpedia.com>
2011-01-14 14:36:21 -0800

    empty application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmg::Application.config.secret_token = '24c8144ce2bb942846612395e23dde57fd1344cafe9492b06b67f028ced90c1dc55dcc0a45b06386e4c683e330656d416c8eae24ba9bca35f59f31f6f8f2af05'
