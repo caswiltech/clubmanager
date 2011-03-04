dcf5c106690129bfbc275efaae58dd7619793eb6
Ryan Leslie <rleslie@partnerpedia.com>
2011-03-03 22:05:06 -0800

    removed associations from user_role.rb

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,6 +1,6 @@
 class UserRole < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :club, :as => :adminable
-  belongs_to :division, :as => :adminable
-  belongs_to :team, :as => :adminable
+  # belongs_to :user
+  # belongs_to :club, :as => :adminable
+  # belongs_to :division, :as => :adminable
+  # belongs_to :team, :as => :adminable
 end
