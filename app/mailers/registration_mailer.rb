9e8559fa9f6da319c470a9844b50238fd6008767
Ryan Leslie <rleslie@partnerpedia.com>
2011-03-25 21:00:40 -0700

    email validation improved intentional error

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -4,7 +4,7 @@ class RegistrationMailer < ActionMailer::Base
   def public_registration(registration)
     @registration = registration
     mail_to = []
-    mail_to << "#{registration.parent_guardian1.first_name} #{registration.parent_guardian1.last_name} <#{registration.parent_guardian1.email}>" unless registration.parent_guardian1.email.blank?
+    mail_to << "#{registration.parent_guardian1.first_name} #{registration.parent_guardian1.last_name} <#{registration.parent_guardian1.email} ERROR!>" unless registration.parent_guardian1.email.blank?
     mail_to << "#{registration.parent_guardian2.first_name} #{registration.parent_guardian2.last_name} <#{registration.parent_guardian2.email}>" unless registration.parent_guardian2.nil? || registration.parent_guardian2.email.blank? || registration.parent_guardian2.email == registration.parent_guardian1.email
     if mail_to.blank?
       mail_to << "EMAIL NOT PROVIDED <#{registration.club.reg_notify_email}>"
