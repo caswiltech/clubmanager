a511b0bb5cb28bfe26e0b45ceb54a27aa9478a4a
Ryan Leslie <rleslie@partnerpedia.com>
2011-04-21 20:46:33 -0700

    removing habtm between clubs and users

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,7 +2,7 @@ class Club < ActiveRecord::Base
   validates_presence_of :long_name, :short_name, :subdomain, :contact_email, :reg_notify_email
   # validates_format_of :image_url, :with => %r{\.(gif|jpg|jpeg|png)$}i, :message => "must be a URL for a GIF, JPG or PNG image."
   has_many :club_logos
-  has_and_belongs_to_many :users
+  # has_and_belongs_to_many :users
   has_many :user_roles, :dependent => :destroy
   has_many :seasons, :dependent => :destroy
   has_many :divisions, :dependent => :destroy
