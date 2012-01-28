614dfd8b365b240064eef131856d7b473bf10957
Ryan Leslie <rleslie@partnerpedia.com>
2012-01-27 20:10:56 -0800

    fool heroku with a minor commit

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 WICKED_PDF = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
+  #:layout => "pdf.html", 
   :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
 }
